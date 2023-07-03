1b706f9ae76063d2744e0d0282c495297759faf1
CyberCondor <116996274+CyberCondor@users.noreply.github.com>
2023-07-02 23:21:12 -0700

    Initial commit

---
+++ Fix-WinVerifyTrustSignatureValidationVuln.ps1
@@ -0,0 +1,31 @@
+﻿$Name = "EnableCertPaddingCheck"
+$Value = "1"
+
+$RegistryPath = "HKLM:\Software\Microsoft\Cryptography\Wintrust\Config"
+if(!(Test-Path $RegistryPath)){
+    New-Item -Path "HKLM:\Software\Microsoft\Cryptography\Wintrust"
+    New-Item -Path $RegistryPath
+    New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value
+}
+elseif((Get-ItemProperty -Path $RegistryPath).$Name -eq "0"){
+    Set-ItemProperty -Path $RegistryPath -Name $Name -Value $Value
+}
+elseif((Get-ItemProperty -Path $RegistryPath).$Name -eq "1"){
+    Get-Item -Path $RegistryPath
+}
+else{New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value}
+
+
+$RegistryPath = "HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust\Config"
+if(!(Test-Path $RegistryPath)){
+    New-Item -Path "HKLM:\Software\Wow6432Node\Microsoft\Cryptography\Wintrust"
+    New-Item -Path $RegistryPath
+    New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value
+}
+elseif((Get-ItemProperty -Path $RegistryPath).$Name -eq "0"){
+    Set-ItemProperty -Path $RegistryPath -Name $Name -Value $Value
+}
+elseif((Get-ItemProperty -Path $RegistryPath).$Name -eq "1"){
+    Get-Item -Path $RegistryPath
+}
+else{New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value}
